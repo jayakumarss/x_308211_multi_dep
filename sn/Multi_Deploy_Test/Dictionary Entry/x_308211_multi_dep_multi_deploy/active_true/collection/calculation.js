feecb3fc1ae249775cb941d64532a25fdb22ef9a
jayakumarss <jayakumar.sengodan@gmail.com>
2019-12-31 10:42:57 -0500

    'master' branch updated from  https://dev66099.service-now.com no-cicd

---
+++ sn/Multi_Deploy_Test/Dictionary Entry/x_308211_multi_dep_multi_deploy/active_true/collection/calculation.js
@@ -0,0 +1,15 @@
+/* 
+ * Application : Multi_Deploy_Test
+ * ClassName   : sys_dictionary
+ * Created On  : 2019-12-31 15:39:34
+ * Created By  : admin
+ * Updated On  : 2019-12-31 15:39:34
+ * Updated By  : admin
+ * URL         : /sys_dictionary.do?sys_id=694fedb9db020010343f58b3ca96197d
+ */
+(function calculatedFieldValue(current) {
+
+	// Add your code here
+	return '';  // return the calculated value
+
+})(current);
