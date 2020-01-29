ce0048e3a50f05d96db980052516b68052fe6dda
jayakumarss <jayakumar.sengodan@gmail.com>
2020-01-29 10:17:57 -0500

    'master' branch updated from  https://dev96952.service-now.com no-cicd

--- sn/Multi_Deploy_Test/Dictionary Entry/x_308211_multi_dep_multi_deploy/active_true/collection/calculation.js
+++ sn/Multi_Deploy_Test/Dictionary Entry/x_308211_multi_dep_multi_deploy/active_true/collection/calculation.js
@@ -3,8 +3,8 @@
  * ClassName   : sys_dictionary
  * Created On  : 2019-12-31 15:39:34
  * Created By  : admin
- * Updated On  : 2020-01-02 15:47:27
- * Updated By  : devops
+ * Updated On  : 2019-12-31 15:39:34
+ * Updated By  : admin
  * URL         : /sys_dictionary.do?sys_id=694fedb9db020010343f58b3ca96197d
  */
 (function calculatedFieldValue(current) {
